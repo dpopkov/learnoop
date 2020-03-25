c8c81acef9e7e82ff905cbc9d6c9ff91e2cc19e1
Denis Popkov <pkvdenis@gmail.com>
2020-03-26 02:37:24 +0300

    ipdppp: ch11: version 20 of Banking demo: usage of TableView

--- jpdppp/src/main/java/learn/oop/jpdppp/bank/gui/AllView.java
+++ jpdppp/src/main/java/learn/oop/jpdppp/bank/gui/AllView.java
@@ -1,10 +1,9 @@
 package learn.oop.jpdppp.bank.gui;
 
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -13,11 +12,11 @@ import learn.oop.jpdppp.bank.BankAccount;
 
 public class AllView {
     private final Pane root;
-    private final ListView<BankAccount> accounts = new ListView<>();
+    private final TableView<BankAccount> accounts = new TableView<>();
 
     public AllView(AllController controller) {
         root = createNodeHierarchy(controller);
-        accounts.setItems(controller.getAccountList());
+        initTableView(controller);
     }
 
     private Pane createNodeHierarchy(AllController controller) {
@@ -33,6 +32,34 @@ public class AllView {
         title.setTextFill(Color.GREEN);
         p1.getChildren().addAll(title, accounts, interestButton);
         return p1;
+    }
+
+    private void initTableView(AllController controller) {
+        TableColumn<BankAccount, Integer> acctNumCol = new TableColumn<>("Account Number");
+        acctNumCol.setCellValueFactory(p -> {
+            BankAccount ba = p.getValue();
+            int acctNum = ba.getAcctNum();
+            return new SimpleObjectProperty<>(acctNum);
+        });
+        TableColumn<BankAccount, String> typeCol = new TableColumn<>("Account Type");
+        typeCol.setCellValueFactory(p -> {
+            BankAccount account = p.getValue();
+            // todo: remove this substring operation after adding method typeName() to BankAccount
+            String s = account.toString().substring(0, account.toString().indexOf(" account "));
+            return new SimpleObjectProperty<>(s);
+        });
+        TableColumn<BankAccount, Integer> balanceCol = new TableColumn<>("Balance");
+        balanceCol.setCellValueFactory(p -> new SimpleObjectProperty<>(p.getValue().getBalance()));
+        TableColumn<BankAccount, String> foreignCol = new TableColumn<>("Owner Status");
+        foreignCol.setCellValueFactory(p -> {
+            boolean isForeign = p.getValue().isForeign();
+            String owner = isForeign ? "Foreign" : "Domestic";
+            return new SimpleObjectProperty<>(owner);
+        });
+        //noinspection unchecked
+        accounts.getColumns().addAll(acctNumCol, typeCol, balanceCol, foreignCol);
+        accounts.setItems(controller.getAccountList());
+        accounts.setPrefSize(380, 240);
     }
 
     public Pane getRoot() {
