e4a7d8ff7ed6bd16390f47aee0708be53432cab2
Denis Popkov <pkvdenis@gmail.com>
2020-07-04 15:20:34 +0300

    linkeddp: complete Builder

--- linkeddp/README.md
+++ linkeddp/README.md
@@ -6,7 +6,7 @@ Creational Patterns
 
 ### Builder
 
-#### Simple Builder:
 [Bedroom](src/main/java/learn/oop/linkeddp/creational/builder/Bedroom.java),
 [BedroomBuilder](src/main/java/learn/oop/linkeddp/creational/builder/BedroomBuilder.java),
 [Architect](src/main/java/learn/oop/linkeddp/creational/builder/Architect.java)  
+[Builder](src/main/java/learn/oop/linkeddp/creational/builder/Builder.java)  
---
+++ linkeddp/src/main/java/learn/oop/linkeddp/creational/builder/Builder.java
@@ -0,0 +1,17 @@
+package learn.oop.linkeddp.creational.builder;
+
+import java.awt.*;
+
+public interface Builder {
+    Builder setDimension(Dimension dimension);
+
+    Builder setCeilingHeight(int ceilingHeight);
+
+    Builder setFloorNumber(int floorNumber);
+
+    Builder setWallColor(Color wallColor);
+
+    Builder setNumberOfWindows(int numberOfWindows);
+
+    Builder setNumberOfDoors(int numberOfDoors);
+}
